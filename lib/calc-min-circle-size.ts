8c674321cffef24dd41c0d9c76bf651090c1a832
Anton Liada <liadaanton@gmail.com>
2024-09-29 21:49:12 +0300

    fix(bubbles): rework bubbles min size

--- lib/calc-min-circle-size.ts
+++ lib/calc-min-circle-size.ts
@@ -10,11 +10,9 @@ const calculateMinCircleSize = () => {
   } else if (width > 1200) {
     return 45;
   } else if (width > 770) {
-    return 50;
-  } else if (width > 550) {
-    return 42;
+    return 55;
   } else {
-    return 30;
+    return 39.5;
   }
 }
 
