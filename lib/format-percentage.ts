5b3a5f57786c3617334c9b98bf34c8463f3e8ebc
Anton Liada <liadaanton@gmail.com>
2024-09-30 23:04:47 +0300

    fix(format-percentage): function was reworked

--- lib/format-percentage.ts
+++ lib/format-percentage.ts
@@ -1,12 +1,16 @@
 const formatPercentage = (num: number) => {
-  if (num >= 1) {
-    return Math.round(num) + 'K';
+  if (num >= 1_000_000) {
+    return (num / 1_000_000).toLocaleString('en-US') + 'M';
+  } else if (num >= 1_000) {
+    return (num / 1_000).toLocaleString('en-US') + 'K';
+  } else if (num >= 1) {
+    return Math.round(num).toLocaleString('en-US');
   } else {
     const decimalPart = num.toString().split('.')[1] || '';
 
     const paddedDecimal = decimalPart.padEnd(4, '0');
 
-    return paddedDecimal[0] + paddedDecimal[1] + ',' + paddedDecimal[2] + paddedDecimal[3];
+    return paddedDecimal[1] + ',' + paddedDecimal[2] + paddedDecimal[3];
   }
 }
 
